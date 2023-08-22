1eb04bac6e0cfddb33506c913d9b44506dbd166b
yincangxiaoqi <142895042+yincangxiaoqi@users.noreply.github.com>
2023-08-23 04:09:59 +0800

    Update tan.js

--- tan.js
+++ tan.js
@@ -1,1 +1,154 @@
-7657767
+{
+ "分享内容":"【胖崽软件库】\n各种破解软件！免费分享！\n下载地址①\nhttps://wwtw.lanzouk.com/s/pzpz2\n下载地址②\n https://pzpz.lanzoub.com/s/pzpz5\n官网地址③\nwww.pzpzb.com\n官网地址④\nwww.pzfxk.com\n复制链接，去浏览器打开，就可以下载！",
+ "群KEY":"https://www.pzpzb.com",
+ "客服QQ":"",
+ "客服微信":"",
+ "启动页图片":"https://tu.xqwt.cc/LightPicture/2023/07/fc70c867ac13df91.jpeg",
+ "启动页跳转":"https://www.pzpzb.com",
+ "启动页标题":"胖崽软件库",
+ "启动页副标题":"胖崽软件库-2023",
+ "启动页跳转时间":"5",
+
+ "公告标题":"",
+ "公告内容":"标题留空不显示弹窗，按钮文字留空不显示按钮，跳转链接留空不跳转",
+ "公告图片": "https://i.szpp.net/2022/10/22/7c94fb7bf86d9.png",
+ "公告按钮一":"按钮一",
+ "公告按钮一跳转":"https://m.baidu.com",
+ "公告按钮二":"按钮二",
+ "公告按钮二跳转":"",
+ "公告按钮三":"按钮三",
+ "公告按钮三跳转":"https://m.baidu.com",
+  
+ "当前版本": "1.3",
+ "更新标题": "【胖崽软件库】\n请更新v1.3新版本\n优化新功能！\n有问题联系胖崽QQ24848177",
+ "更新内容": "请大家更新，新版本！",
+ "强制更新": "开启",
+ "更新按钮一":"点击更新1",
+ "更新按钮一跳转":"https://lanzoup.com/s/pzrjk1",
+ "更新按钮二":"点击更新2",
+ "更新按钮二跳转":"https://lanzoui.com/s/pzrjk2",
+ "更新按钮三":"点击更新3",
+ "更新按钮三跳转":"https://qsxfxg.lanzoue.com/s/pzrjk4",
+
+ "界面一标题": "【首页】",
+ "界面一副标题": "欢迎使用胖崽软件库",
+ "界面一文字": "【官方公告】",
+ "界面二标题": "【软件下载】",
+ "界面二副标题": "软件都是破解版，免费使用",
+ "界面三标题": "【发现】",
+ "界面三副标题": "",
+ "界面四标题": "【我的】",
+ "界面四副标题": "",
+ "界面四文字一": "胖崽软件库",
+ "界面四文字二": "v1.3",
+ "界面四文字三": "免费使用",
+ "界面四远程一": "广告合作",
+ "界面四远程一网址": "https://ts.xqwt.cc/index.php/3.html",
+ "界面四远程二": "永久地址",
+ "界面四远程二网址": "https://www.pzpzb.com",
+ "软件分类": [
+  {
+   "标题": "开車软件",
+   "任务": "关闭",
+   "链接": "https://wwf.lanzoul.com/b04qbu73c",
+   "置顶": [
+    {
+     "name_all": "绳师视频破解",
+     "ico": "https://tu.xqwt.cc/LightPicture/2023/08/2f21132bf2e20558.jpg",
+     "url": "https://ss.ptzg09.xyz/?channel=sdla5-2",
+     "time": "极.品视频"
+    } ,
+    {
+     "name_all": "R星视频",
+     "ico": "https://tu.xqwt.cc/LightPicture/2023/08/07ca74e76284027e.jpg",
+     "url": "https://rx.rxs002.cc/?ch=ty001",
+     "time": "精品软件 (无限观看)"
+    } ,
+    {
+     "name_all": "52视频",
+     "ico": "https://tu.xqwt.cc/LightPicture/2023/08/960c916fe5588f7c.jpg",
+     "url": "https://api.cshun.cn/loading52.html?c=vdla1-18",
+     "time": "超级好看"
+    }
+   ]
+  },
+  {
+   "标题": "开車游戏",
+   "任务": "关闭",
+   "链接": "https://wwf.lanzoul.com/b04qbw5pe",
+   "置顶": [
+     
+   ]
+  },
+  {
+   "标题": "普通软件",
+   "任务": "关闭",
+   "链接": "https://wwjx.lanzout.com/b04q4p92b",
+   "置顶": [
+     
+   ]
+  },
+  {
+   "标题": "ṾṖṄ软件",
+   "任务": "关闭",
+   "链接": "https://wwf.lanzoul.com/b04qbw74f",
+   "置顶": [
+     
+   ]
+  }
+ ],
+ "首页轮播图": [
+  {
+   "标题": "胖崽软件库",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/ec7e817a97ce918b.jpg",
+   "链接": "https://www.pzpzb.com"
+  }
+  
+ ],
+ "官方通知": [
+  {
+   "标题": "永久下载地址【必看】",
+   "时间": "2023-01-01 00:00:00",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/6a673ba2eb75ae48.jpg",
+   "链接": "https://ts.xqwt.cc/index.php/15.html"
+  },
+   {
+   "标题": "软件库【使用教程】",
+   "时间": "2023-01-01 00:00:00",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/bb3c247a97043047.jpeg",
+   "链接": "https://ts.xqwt.cc/index.php/14.html"
+  }, 
+  {
+   "标题": "【软件不能下载】【软件不能安装】解决办法",
+   "时间": "2023-01-01 00:00:00",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/d556d37784a955cd.jpg",
+   "链接": "https://ts.xqwt.cc/index.php/11.html"
+  },
+  {
+   "标题": "公告声明",
+   "时间": "2023-01-01 00:00:00",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/af249034e20de9b7.jpg",
+   "链接": "https://ts.xqwt.cc/index.php/4.html"
+  },
+  {
+   "标题": "广告合作",
+   "时间": "2023-01-01 00:00:00",
+   "图片": "https://tu.xqwt.cc/LightPicture/2023/07/ee4aa69dc8ea6446.jpeg",
+   "链接": "https://ts.xqwt.cc/index.php/3.html"
+  }
+ ],
+ "发现": [
+  {
+   "标题": "胖崽软件库【永久下载地址】",
+   "链接": "https://ts.xqwt.cc/index.php/15.html"
+  },
+    
+  {
+   "标题": "【问题反馈】",
+   "链接": "https://ts.xqwt.cc/index.php/25.html"
+  }
+   ],
+ "软件列表置顶": [
+  
+ ]
+}
